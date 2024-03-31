0c40759af31421602b914bded8e1191bcd2ad921
Abouzaid1 <118759178+Abouzaid1@users.noreply.github.com>
2024-03-31 04:45:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,18 +16,27 @@ const { createServer } = require('node:http');
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 io.on('connect', (socket) => {
-    console.log('A user connected');
-    
+    socket.on('dataFromClient', (data) => {
+        const { room, message } = data;
+        // io.to(room).emit('message', message);
+        console.log(room);
+        socket.join(room);
+    });
     // Listen for mouseMove events from clients
     socket.on('mouseMove', (data) => {
         // Broadcast the mouse movement data to all connected clients except the sender
         socket.broadcast.emit('mouseMove', data);
     });
-    
+    socket.on('controllerMove', (data) => {
+        // Broadcast the mouse movement data to all connected clients except the sender
+        socket.broadcast.emit('controllerMove', data);
+    });
+
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
 });
+
 
 server.listen(10000, () => {
     console.log('server running at 3000');
